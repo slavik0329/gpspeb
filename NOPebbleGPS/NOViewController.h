16c0ef2b6af3866bc889b67c1c5acf257e09d783
Nathan Oates <cazlar@Njord.local>
2013-05-31 21:25:04 +1000

    Initial Commit

---
+++ NOPebbleGPS/NOViewController.h
@@ -0,0 +1,13 @@
+//
+//  NOViewController.h
+//  NOPebbleGPS
+//
+//  Created by Nathan Oates on 31/05/13.
+//  Copyright (c) 2013 Nathan Oates. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NOViewController : UIViewController
+
+@end
---
+++ NOPebbleGPS/NOViewController.m
@@ -0,0 +1,29 @@
+//
+//  NOViewController.m
+//  NOPebbleGPS
+//
+//  Created by Nathan Oates on 31/05/13.
+//  Copyright (c) 2013 Nathan Oates. All rights reserved.
+//
+
+#import "NOViewController.h"
+
+@interface NOViewController ()
+
+@end
+
+@implementation NOViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
